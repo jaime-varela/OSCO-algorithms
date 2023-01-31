7b6b572f4c660aeffae8a5d0e2f610562aa6ff31
jaime-varela <varela.jaime01@gmail.com>
2023-01-31 00:14:42 -0500

    non-working boiler plate code

--- algorithms/general_stochastic_scheduler.hpp
+++ algorithms/general_stochastic_scheduler.hpp
@@ -1,12 +1,22 @@
 #pragma once
 #include "osco_concepts.hpp"
+#include "osco_data_structures.hpp"
 
 namespace OSCO
 {
-    double add(double a,double b) {
-        return a+b;
+    
+    // Generic algorithm Figure 2.1
+    template <class RequestT,IndexT,class ChooseFunction>
+    myType Online_Algorithm (OrdereredRequestSet<RequestT> requests, IndexT t_start, IndexT t_end, ChooseFunction Choose_Requests) 
+    {
+        auto current_schedule = Schedule<RequestT>();
+        for (IndexT t_step = t_start; t_step <= t_end; t_step++)
+        {
+            auto chosen_request = Choose_Requests(current_schedule,requests.sub_sequence(t_start,t_step));
+            current_schedule = current_schedule | chosen_request;
+        }
+        return current_schedule;
     }
-    // TODO: 
 
 
 } // namespace OSCO
